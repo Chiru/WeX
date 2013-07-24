6d1124fba86ebb2f6b9a1f75c7d21b9c9ca93917
Juha Hyvärinen <juha.hyvarinen@cie.fi>
2013-07-24 15:23:50 +0300

    Demo4 backend: Move OpenPOIs database search to another thread.

--- demos/demo4/backend/backendServer.py
+++ demos/demo4/backend/backendServer.py
@@ -3,7 +3,7 @@
 
 import sys
 import httplib2
-import json
+import threading
 
 from xml.etree import ElementTree as ET
 from twisted.internet import reactor
@@ -36,8 +36,8 @@ def parseXmlDocument(document):
         message["msg"] = etree.find("msg").text
         message["query"] = etree.find("query").text
 
-        data["error"] = message
-        return json.dumps(str(data).replace('\'', '"'))
+        data.append(message)
+        return '{"Error":' + (str(data).replace('\'', '"')) + '}'
 
     for poiElement in etree.findall("poi"):
         poi = dict()
@@ -57,28 +57,36 @@ def parseXmlDocument(document):
             poi["location"] = poiLocation
  
         #print poi
-
         data.append(poi)
 
     return '{"pois":' + str(data).replace('\'', '"') + '}'
 
 
 class MessageBasedHashServerProtocol(WebSocketServerProtocol):
-    def onMessage(self, search, binary):
-        # self.sendMessage(msg, binary)
+    def onOpen(self):
+        print ("onOpen")
 
-        responseMessage = doPoiSearch(search)
-        parsedResponse = parseXmlDocument(responseMessage)
+    def onMessage(self, search, binary):
+
+        class ActionThread(threading.Thread):
+            def __init__(self, protocol):
+                threading.Thread.__init__(self)
+                self.protocol = protocol
+            
+            def run(self):
+                responseMessage = doPoiSearch(search)
+                parsedResponse = parseXmlDocument(responseMessage)
+                print (parsedResponse)
+                reactor.callFromThread(self.protocol.sendMessage, parsedResponse)
+
+        ActionThread(self).start()
+
 
         # Used for development since searching from OpenPOIS database was so slow
         # with open ("poiquery.xml", "r") as myfile:
         #     data=myfile.read().replace('\n', '')
         # parsedResponse = parseXmlDocument(data)
-        
-        print (parsedResponse)
-        self.sendMessage(parsedResponse)
-
-    
+            
 
 if __name__ == '__main__':
  
@@ -86,6 +94,7 @@ if __name__ == '__main__':
  
     factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
     factory.protocol = MessageBasedHashServerProtocol
+    factory.setProtocolOptions(allowHixie76 = True)
     listenWS(factory)
- 
+
     reactor.run()
