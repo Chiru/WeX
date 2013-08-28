b6d2500e9ab556c900d55d8c3d255dfc884bbf9d
Toni Dahl <toni.dahl@cie.fi>
2013-08-28 12:34:52 +0300

    Changes in OpenPOI data mapping to JSON. Trying out our own JSON POI structure.

--- demos/demo4/backend/backendServer.py
+++ demos/demo4/backend/backendServer.py
@@ -4,13 +4,12 @@
 import sys
 import httplib2
 import threading
+import json
 
 from xml.etree import ElementTree as ET
 from twisted.internet import reactor
 #from twisted.python import log
-from autobahn.websocket import WebSocketServerFactory, \
-                               WebSocketServerProtocol, \
-                               listenWS
+from autobahn.websocket import WebSocketServerFactory, WebSocketServerProtocol, listenWS
 
 RequestBaseURL = "http://openpois.net/poiquery.php?"
 
@@ -19,7 +18,7 @@ def doPoiSearch(search):
     global RequestBaseURL
     http = httplib2.Http()
     requestUrl = RequestBaseURL + search
-    
+
     print ("Doing search with requestUrl: " + requestUrl)
     response, content = http.request(requestUrl, 'POST')
 
@@ -28,7 +27,7 @@ def doPoiSearch(search):
 
 def parseXmlDocument(document):
     etree = ET.fromstring(document)
-    data = []
+    data = dict()
 
     if etree.tag == "Error":
         print "Error in search"
@@ -36,31 +35,52 @@ def parseXmlDocument(document):
         message["msg"] = etree.find("msg").text
         message["query"] = etree.find("query").text
 
-        data.append(message)
-        return '{"Error":' + (str(data).replace('\'', '"')) + '}'
+        return '{"Error":' + (str(message).replace('\'', '"')) + '}'
 
+    # Iterating through POI elements in the XML and mapping the data into JSON object
     for poiElement in etree.findall("poi"):
-        poi = dict()
+        uuid = poiElement.get("id")
+        if uuid is None:
+            continue
 
+        poi = dict()
+        contents = []
+        locations = []
+
+        poi["source"] = poiElement.get("base", "")
+
+        # Parsing contents
         label = poiElement.find("label")
         if label is not None:
-            poi["name"] = label.find("value").text
+            tmp = dict(type="name", lang="en-UK")
 
-        info = poiElement.find("category")
-        if info is not None:
-            # Hack to remove unneeded ' characters from result string, since dataformat in OpenPOI's database have changed after original code was written
-            poi["info"] = info.find("value").text.replace('\'','')
+            if label.get("term") is not None:
+                tmp["term"] = label.get("term")
 
+            contents.append(tmp)
+
+        category = poiElement.find("category")
+        if category is not None:
+            # Line below has a hack to remove unneeded ' characters from result string,
+            # since data format in OpenPOI's database have changed after original code was written
+            tmp = dict(type="category", lang="en-UK", value=category.find("value").text.replace("'", ""))
+            if category.get("scheme") is not None:
+                tmp["scheme"] = category.get("scheme")
+
+            contents.append(tmp)
+
+        # Parsing locations
         location = poiElement.find("location")
         if location is not None:
-            poiLocation = dict()
-            poiLocation["lat"], poiLocation["lon"] = location.find("point").find("Point").find("posList").text.split()
-            poi["location"] = poiLocation
- 
-        #print poi
-        data.append(poi)
+            lat, lon = location.find("point").find("Point").find("posList").text.split()
+            locations.append(dict(type="wsg84", lat=lat, lon=lon))
 
-    return '{"pois":' + str(data).replace('\'', '"') + '}'
+        # Map POI to JSON
+        poi["contents"] = contents
+        poi["locations"] = locations
+        data[uuid] = poi
+
+    return json.dumps({"pois": data}, indent=2)
 
 
 class MessageBasedHashServerProtocol(WebSocketServerProtocol):
@@ -68,12 +88,11 @@ class MessageBasedHashServerProtocol(WebSocketServerProtocol):
         print ("onOpen")
 
     def onMessage(self, search, binary):
-
         class ActionThread(threading.Thread):
             def __init__(self, protocol):
                 threading.Thread.__init__(self)
                 self.protocol = protocol
-            
+
             def run(self):
                 responseMessage = doPoiSearch(search)
                 parsedResponse = parseXmlDocument(responseMessage)
@@ -87,15 +106,14 @@ class MessageBasedHashServerProtocol(WebSocketServerProtocol):
         # with open ("poiquery.xml", "r") as myfile:
         #     data=myfile.read().replace('\n', '')
         # parsedResponse = parseXmlDocument(data)
-            
+
 
 if __name__ == '__main__':
- 
     #log.startLogging(sys.stdout)
- 
-    factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+
+    factory = WebSocketServerFactory("ws://localhost:9000", debug=False)
     factory.protocol = MessageBasedHashServerProtocol
-    factory.setProtocolOptions(allowHixie76 = True)
+    factory.setProtocolOptions(allowHixie76=True)
     listenWS(factory)
 
     reactor.run()
