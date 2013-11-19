311de274b4c9bbf120fd426af4de60b642b249c7
A-K <anttikarh@gmail.com>
2013-11-19 12:46:21 +0200

    New functionality added to AR manager.

--- ar/ARManager.js
+++ ar/ARManager.js
@@ -1,9 +1,5 @@
 // For conditions of distribution and use, see copyright notice in LICENSE
 
-/**
- * @author Toni Dahl
- */
-
 
 (function ( namespace, undefined ) {
     "use strict";
@@ -22,43 +18,61 @@
         // Setting options
         opts = extend( {}, defaults, options );
 
-        var flowAnalysers, observers, background, bgCtx;
+        var flowAnalysers, observers, background, bgCtx, callBackFunctions = [];
 
         flowAnalysers = {
             'ARBase': false,
-            'QRAnalyser': false
+            'MarkerAnalyser': false
         };
 
         observers = {
             'ARBase': videoStreamObserver(),
-            'QRAnalyser': QRObserver()
+            'MarkerAnalyser': MarkerObserver()
         };
+        
+        var video, localVideoStream, alvar;
+
+
+        var noCameraFeedError = function () {
+            log( "ARManager: ERROR: No camera feed available." );
+        };
+
+        var noUserMediaError = function () {
+            log( "ARManager: ERROR: No navigator.getUserMedia method available, check if your browser supports WebRTC." );
+        };
+
 
         function videoStreamObserver() {
             return new XML3DDataObserver( function ( records, observer ) {
                 var arVideo = records[0].result.getValue( "arvideo" ),
                     imageData, i, dataLen, width, height;
-
+                    
                 if ( arVideo && bgCtx) {
-
+                
                     width = arVideo.width;
                     height = arVideo.height;
-
+         
                     // Setup background canvas
                     if ( width !== background.width || height !== background.height ) {
                         background.width = width;
                         background.height = height;
                     }
-                
                     bgCtx.putImageData( arVideo, 0, 0 );
                 }
             } );
         }
 
-        function QRObserver() {
+        function MarkerObserver() {
             return new XML3DDataObserver( function ( records, observer ) {
-                var transforms = records[0].result.getValue( "transforms" );
-                //TODO: Utilise results from QR tag analyser to perform transforms to objects by using scenemanager
+                var i, transforms = records[0].result.getValue("transforms");
+                var visibilities = records[0].result.getValue("visibilities");
+                
+                    if(callBackFunctions.length === 0)
+                        return;
+                        
+                    for(i in callBackFunctions) {
+                        callBackFunctions[i](transforms, visibilities);
+                    }
             } );
         }
 
@@ -80,9 +94,9 @@
                     if ( id === 'ARBase' ) {
                         observers[id].observe( flowAnalysers[id], {names: ["arvideo"]} );
                     }
-                    else if ( id === 'QRAnalyser' ) {
-                        log( "ARManager: Observing QR tags from the camera feed." );
-                        observers[id].observe( flowAnalysers[id], {names: ["transforms"]} );
+                    else if ( id === 'MarkerAnalyser' ) {
+                        log( "ARManager: Observing Marker tags from the camera feed." );
+                        observers[id].observe( flowAnalysers[id], {names: ["transforms", "visibilities"]} );
                     }
                 }
             }
@@ -91,16 +105,58 @@
         this.init = function () {
             log( "ARManager: Initialising..." );
 
-            background = document.getElementById( 'background' );
+            window.URL = window.URL || window.webkitURL;
+            navigator.getUserMedia = (navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia);
 
-            bgCtx = background.getContext( '2d' );
+            video = document.querySelector( 'video' );
+
+            if ( !video ) {
+                log( "ARManager: ERROR: No video tag was found." );
+                return;
+            }
+
+            this.getCameraFeed();
+
+            background = document.getElementById( 'background' );
+            
+            if(background)
+                bgCtx = background.getContext( '2d' );
 
             initFlowAnalysers();
+            alvar = new AR.Alvar(framework);
             initObservers();
 
             log( "ARManager: Done." );
 
         };
+        
+        this.hasGetUserMedia = function () {
+            return (navigator.getUserMedia);
+        };
+
+        this.getCameraFeed = function () {
+            if ( this.hasGetUserMedia() ) {
+                log( "ARManager: Requesting Camera feed." );
+
+                navigator.getUserMedia( {video: true, audio: false}, function ( stream ) {
+                    video.src = window.URL.createObjectURL( stream );
+                    localVideoStream = stream;
+                    log( "ARManager: Got camera feed. url: " + video.src);
+                    video.play();
+                    
+                }, noCameraFeedError );
+            } else {
+
+                noUserMediaError();
+            }
+        };
+        
+        this.setMarkerCallback = function(callback) {
+            callBackFunctions.push(callback);
+        }
 
         this.init();
 
