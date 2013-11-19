ded087879ec8bf2084258f0322e8a3c582549b53
A-K <anttikarh@gmail.com>
2013-11-19 12:48:49 +0200

    New functionality added.

--- ar/SceneManager.js
+++ ar/SceneManager.js
@@ -1,10 +1,5 @@
 // For conditions of distribution and use, see copyright notice in LICENSE
 
-/**
- * @author Toni Dahl, Antti Karhu
- */
-
-
 (function ( namespace, undefined ) {
     "use strict";
 
@@ -17,40 +12,37 @@
 
     var SceneManager = AR.SceneManager = function ( framework, options ) {
 
-        var defaults = {},
-            opts;
+        var defaults = {}, opts;
 
         // Setting options
         opts = extend( {}, defaults, options );
 
-        var scene, visibleObjects = [], activeCamera, xml3d, cameraVerticalPlane = 90;
+        var scene, visibleObjects = [], activeCamera, xml3d, cameraVerticalPlane = 65, billboardSet = [], 
+            maxDistance = 50, transIndex = 0, up = new XML3DVec3(0,1,0);
 
         this.init = function () {
             log( "SceneManager: Initialising..." );
+            xml3d = document.querySelector("xml3d");
             this.initCamera();
         };
 
 
-        // TODO: These methods are subject to change, until we know for sure what is the best way to add 3d object
-        // dynamically to xml3d scene
+        this.addToScene = function (Poi_fw_xml3d_component) {
 
-        this.addToScene = function (object3D){
-
+            if (!Poi_fw_xml3d_component) {
+                return null;
+            }
+                         
+            if(Poi_fw_xml3d_component.hasOwnProperty( "model" )) {
+                $("xml3d").append(Poi_fw_xml3d_component['model']);
+            }
         };
 
-    
-        this.removeFromScene = function (object3D) {
-
+        this.addObjetcToBillboardSet = function(xml3dElement) {
+            billboardSet.push(xml3dElement);
         };
-
-
-        this.getObjectByID = function(id){
-
-        };
-
 
         this.initCamera = function(){
-            xml3d = document.querySelector("xml3d");
             activeCamera = XML3D.URIResolver.resolveLocal(xml3d.getAttribute("activeView"), xml3d.ownerDocument);
         };
 
@@ -58,24 +50,53 @@
             return activeCamera;
         };
         
-         this.setPositionFromGeoLocation = function (curLoc, elemLoc, xml3dElement) {
+        this.setPositionFromGeoLocation = function (curLoc, elemLoc, xml3dElement) {
 
-            var transformURL, transform, radius, maxDistance = 100;
+            var transform, radius;
+            transform = XML3D.URIResolver.resolveLocal(xml3dElement.getAttribute("transform"), xml3dElement.ownerDocument);
+
+            if(transform === null) {
+                 transform = XML3D.createElement("transform");
+                 transform.setAttribute("id", "transform" + transIndex);
+                 xml3d.appendChild(transform);
+                 xml3dElement.setAttribute("transform", "#transform" + transIndex);
+                 transIndex++;
+            }
+            
+            var geolocation = distanceBetween(curLoc, elemLoc);
+            
+            radius = Math.min(geolocation.distance, maxDistance);
+
+            transform.translation.z = activeCamera.position.z + radius * Math.cos(Math.PI - geolocation.bearing);
+            transform.translation.x = activeCamera.position.x + radius * Math.sin(Math.PI - geolocation.bearing);
+            //log("distance: " + geolocation.distance.toFixed( 2 ) + " meters,  radius: " + radius);
+            log("bearing: " + geolocation.bearing * (180 / Math.PI) + " degrees");
+            log("location x: " + transform.translation.x + "  z: " + transform.translation.z);
+
+        };
+        
+        this.setTransformFromMarker = function(markerTransform, xml3dElement) {
+        
+            var transform, mat3x3 = [], quat = XML3D.math.quat.create();
+            
+            if(!markerTransform || !xml3dElement)
+                return;
+
             transform = XML3D.URIResolver.resolveLocal(xml3dElement.getAttribute("transform"), xml3dElement.ownerDocument);
 
             if(transform === null)
                 return;
+                
+            //XML3D.math.mat4.toMat3(transforms, mat3x3);
+            mat3x3[0] = markerTransform[0], mat3x3[1] = markerTransform[4], mat3x3[2] = markerTransform[8],
+            mat3x3[3] = markerTransform[1], mat3x3[4] = markerTransform[5], mat3x3[5] = markerTransform[9],
+            mat3x3[6] = markerTransform[2], mat3x3[7] = markerTransform[6], mat3x3[8] = markerTransform[10];
+            XML3D.math.quat.setFromMat3(mat3x3, quat);
             
-            var geolocation = distanceBetween(curLoc, elemLoc);
-            
-            radius = 10;//Math.min(geolocation.distance, maxDistance);
-            log("distance: " + geolocation.distance.toFixed( 2 ) + " meters,  radius: " + radius);
-            log("bearing: " + geolocation.bearing * (180 / Math.PI) + " degrees");
+            transform.rotation._setQuaternion(XML3D.math.quat.multiply(quat, activeCamera.orientation.getQuaternion(), quat));
+            transform.translation.set(activeCamera.orientation.rotateVec3(new XML3DVec3(markerTransform[12], markerTransform[13], markerTransform[14])));
 
-            transform.translation.z = radius * Math.cos(geolocation.bearing);
-            transform.translation.x = radius * Math.sin(geolocation.bearing);
-            //log("location x: " + transform.translation.x + "  z: " + transform.translation.z);
-        }
+        };
         
         this.setCameraOrientation = function (deviceOrientationEvent) {
 
@@ -100,17 +121,14 @@
 		    rotY = cosX * sinY * cosZ + sinX * cosY * sinZ,
 		    rotZ = cosX * cosY * sinZ - sinX * sinY * cosZ,
 		    rotW = cosX * cosY * cosZ + sinX * sinY * sinZ;
-		    	
+		    
 		    activeCamera.orientation.setQuaternion(new XML3DVec3(rotX, rotY, rotZ), rotW);	
-        }
+		    updateBillboardSet();
+        };
         
         this.translateCamera = function (curLoc, gpsPoint) {
-            //var delta = distanceBetween(curLoc, gpsPoint);
-            //var str = 10 + " " + activeCamera.position.y + " " + activeCamera.position.z;
-            //activeCamera.setAttribute("position", str);
-            //activeCamera.position = activeCamera.position.add(new XML3DVec3(delta, 0.0, 0.0));
-            //log("cameraPos: x: " + activeCamera.position.x + " y: " + activeCamera.position.y + " z: " + activeCamera.position.z);
-        }
+
+        };
         
         //Bearign(radians) and distance between two points(meters) on a sphere using the spherical law of cosines. 
         function distanceBetween(p1, p2) {
@@ -132,10 +150,31 @@
             return result;
         }
 
+        function updateBillboardSet() {
+       
+            var billboard, transform, targetX, targetY, targetZ, upVector;
+            
+            if(billboardSet.length === 0)
+                return;
+            
+            for(billboard in billboardSet) {
+                
+                transform = XML3D.URIResolver.resolveLocal(billboardSet[billboard].getAttribute("transform"), billboardSet[billboard].ownerDocument);    
+                
+                targetZ = activeCamera.position.subtract(transform.translation);
+                targetZ = targetZ.normalize();
+                
+                targetX = up.cross(targetZ);
+                targetX = targetX.normalize();
+        
+                targetY = targetZ.cross(targetX);
+                targetY = targetY.normalize();
+                transform.rotation.setFromBasis(targetX, targetY, targetZ);
+                
+            }
+        }
 
         this.init();
-
-
     };
 
 
